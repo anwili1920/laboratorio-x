433a085b1bfc0599e58807249a7497e4400ecb87
ubuntu20 <angie.centeno@pucp.edu.pe>
2021-09-07 23:03:35 -0500

    funcionalgo

--- Practico lab1/MediaMenor1/asmMedia32.asm
+++ Practico lab1/MediaMenor1/asmMedia32.asm
@@ -1,7 +1,7 @@
 ;extern int asmMedia32(int *a, int N);
 ;     eax es el return
 section .data
-    elmenor  dd 0
+    N  dd 0
 global asmMedia32
 section	.text
     asmMedia32:
@@ -21,7 +21,8 @@ section	.text
     mov ecx,[ebp + 12]  ; ecx <- N
 ; 12 10 11 9 
     mov eax,[edi]   ; menor <- arr[0] cargamos en eax la primera posición del arreglo
-    mov ebx,[ecx]     ; edx ← N
+    mov [rel N],ecx     ; edx ← N
+    mov ebx,ecx
     dec ecx         ; ecx tiene el valor del tamaño del arreglo por lo que lo usamos de contador decreciente 
     jz exit         ; saltamos hacia exit en caso el tamaño del arreglo de datos sea cero 
 elMenor: ;Buscare el menor de los elementos en el loop
@@ -33,29 +34,22 @@ continuar1:
     jmp limpiar
 nuevomenor: 
     mov eax,[edi]
-    jmp continuar1 ; 
+    jmp continuar1
 limpiar:
-    ;mov [rel elmenor],eax
-    xor ecx,ecx
-    ;xor ebx,ebx
-    mov ecx,[ebx]
-operacion:
-    cmp eax,[edi]
-    jne suma
-continuar2:
-    sub edi,4       ; incrementamos la posición del puntero. Recordar que un entero tiene 4 bytesadd edi,4
-    loop operacion
-    jmp exit
-
-suma: 
-    add ebx,[edi]   ; leemos el siguiente valor y almacenamos la suma acumulada en eax
-    jmp continuar2   ; loop para leer todos los valores del arreglo. La instrucción loop trabaja con el registro ecx para saber cuando saltar. Revisar ISA
-
-
+    mov ecx,ebx
+    mov edi,[ebp + 8]
+    mov edx,eax  ; aqui guardo al menor 
+suma:
+    add edi,4       ; incrementamos la posición del puntero. Recordar que un entero tiene 4 bytes
+    add eax,[edi]   ; leemos el siguiente valor y almacenamos la suma acumulada en eax
+    loop suma       ; loop para leer todos los valores del arreglo. La instrucción loop trabaja con el registro ecx para saber cuando saltar. Revisar ISA
 
 exit:
-    mov eax,[ebx]
+    sub eax,edx
+    dec ebx
     cdq              ; extender el signo de eax hacia edx 
+    idiv ebx         ; cociente: eax <- (edx:eax)/ebx ---- residuo: edx
+    ;mov eax,[ebx]
     ;idiv edx         ; cociente: eax <- (edx:eax)/ebx ---- residuo: edx
     ; Epílogo de calling conventions de 32 bits
     pop ebx
