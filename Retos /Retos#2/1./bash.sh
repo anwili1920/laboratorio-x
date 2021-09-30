d52292a6501068ebbc9d89e6679c979f1b3f8a0f
ubuntu20 <angie.centeno@pucp.edu.pe>
2021-09-30 00:23:39 -0500

    retos

--- Retos /Retos#2/1./bash.sh
+++
@@ -1,4 +0,0 @@
-nasm -g -f elf64 Calculo_varianza.asm -o Calculo_varianza.o
-ld -g Calculo_varianza.o -o ejecutable 
-./ejecutable
-gdb -x depurador.gdb
--- Retos /Retos#2/2./bash.sh
+++
@@ -1,4 +0,0 @@
-nasm -g -f elf64 calculo_limite.asm -o calculo_limite.o
-ld -g calculo_limite.o -o ejecutable 
-./ejecutable
-gdb -x depurador.gdb
